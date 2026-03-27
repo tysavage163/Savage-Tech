9184c352ecd52836eba8a55735db27439404dc61
tysavage163 <arcadeskype884@gmail.com>
2026-03-27 12:54:50 +0300

    Delete commands directory

--- commands/commands/admin/commands/admin/link.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'link',
-    async execute(sock, m) {
-        if (!m.key.remoteJid.endsWith('@g.us')) return;
-        const code = await sock.groupInviteCode(m.key.remoteJid);
-        await sock.sendMessage(m.key.remoteJid, { text: `https://chat.whatsapp.com/${code}` });
-    }
-};

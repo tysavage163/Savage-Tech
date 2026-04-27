1946517bf4283677409aa396dbbc6e214eba4e2b
tysavage163 <arcadeskype884@gmail.com>
2026-04-27 10:58:44 +0300

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,15 +1,15 @@
 module.exports = {
     name: "unmute",
     category: "group",
-    async execute(sock, msg, args, { isMe }) {
+    async execute(sock, msg, args) {
         const from = msg.key.remoteJid;
-        if (!isMe || !from.endsWith('@g.us')) return;
+        if (!from.endsWith('@g.us')) return;
 
         try {
             await sock.groupSettingUpdate(from, 'not_announcement');
-            await sock.sendMessage(from, { text: "🔓 **SΛVΛGΞ:** Group Unmuted." });
+            await sock.sendMessage(from, { text: "🔓 **SΛVΛGΞ-TECH:** Group Unmuted." });
         } catch (e) {
-            await sock.sendMessage(from, { text: "❌ **FAIL:** Ensure I am Admin." });
+            await sock.sendMessage(from, { text: "❌ **ADMIN REQUIRED:** Elevate the bot to Admin." });
         }
     }
 };

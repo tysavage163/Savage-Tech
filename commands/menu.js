d7950a22d398d6cb3a63f1d4814f10088b0e4bb1
tysavage163 <arcadeskype884@gmail.com>
2026-05-01 23:39:02 +0300

    Update menu.js

--- commands/menu.js
+++ commands/menu.js
@@ -50,9 +50,10 @@ module.exports = {
             const financialMenu = getCategorizedMenu('financial data', 'FINANCIAL DATA');
             const searchMenu = getCategorizedMenu('search menu', 'SEARCH MENU');
             const animeMenu = getCategorizedMenu('anime', 'ANIME MENU');
+            const ethicalMenu = getCategorizedMenu('ethical hacking', 'ETHICAL HACKING');
 
             // Updated exclusion list (includes new categories)
-            const definedCats = ['owner', 'group', 'ai', 'fun', 'tools', 'download', 'audio', 'engine', 'Audio Effects', 'Audio', 'financial data', 'search menu', 'anime'];
+            const definedCats = ['owner', 'group', 'ai', 'fun', 'tools', 'download', 'audio', 'engine', 'Audio Effects', 'Audio', 'financial data', 'search menu', 'anime', 'ethical hacking'];
 
             // Catch-all for other categories
             const otherMenu = Array.from(global.commands.values())
@@ -63,10 +64,10 @@ module.exports = {
             const footer = `_master your tools or be mastered by them_`;
             
             // Added new menus to the concatenation
-            const fullMenu = header + ownerMenu + groupMenu + aiMenu + funMenu + toolsMenu + downloadMenu + audioMenu + audioEffectsMenu + spotifyMenu + financialMenu + searchMenu + animeMenu + engineMenu + otherMenu + footer;
+            const fullMenu = header + ownerMenu + groupMenu + aiMenu + funMenu + toolsMenu + downloadMenu + audioMenu + audioEffectsMenu + spotifyMenu + financialMenu + searchMenu + animeMenu + ethicalMenu + engineMenu + otherMenu + footer;
 
             await sock.sendMessage(from, { 
-                image: { url: 'https://i.supaimg.com/57b03ae1-422b-4801-b5d2-661ece6d38ae/3672838c-1a31-4c1f-be38-9a080e3f8e1c.png' }, 
+                image: { url: 'https://i.supaimg.com/57b03ae1-422b-4801-b5d2-661ece6d38ae/775997ad-981a-4f09-a861-a18b6cb6888d.png' }, 
                 caption: fullMenu,
                 mentions: [msg.key.participant || from]
             }, { quoted: msg });
